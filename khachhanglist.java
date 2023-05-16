c619258ba5010b9b4940091a4ce9dfd0fb436e0b
duy010203N <125200444+duy010203N@users.noreply.github.com>
2023-05-16 10:56:47 +0700

    Create khachhanglist.java

---
+++ khachhanglist.java
@@ -0,0 +1,25 @@
+package BAI2;
+
+public class khachhanglist {
+	khachhang kh[]= new khachhang[100];
+	int countkh,sum1=0,sum2=0,dem=0,sumtien=0;
+	public khachhanglist() {
+		countkh=0;
+		for(int i=0;i<100;i++) {
+			kh[i] = new khachhang();
+		}
+	}
+	public void themkh(int temp)
+	{
+		if(countkh>100)
+		{
+			System.out.println("day");
+		}
+		else {
+			if(temp==1)
+			{
+				kh[countkh] = new
+			}
+		}
+	}
+}
